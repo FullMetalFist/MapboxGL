77358da7362f3cc1090bf50d9126e781fc3c371d
Michael Vilabrera <m.vilabrera@gmail.com>
2015-05-22 11:33:49 -0400

    functional map with MGLAccountManager implemented

---
+++ PlayingWithMapboxGL/Constants.h
@@ -0,0 +1,15 @@
+//
+//  Constants.h
+//  PlayingWithMapboxGL
+//
+//  Created by Michael Vilabrera on 5/22/15.
+//  Copyright (c) 2015 Giving Tree. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Constants : NSObject
+
+extern NSString *const API_ACCESS_TOKEN;
+
+@end
--- PlayingWithMapboxGL/ViewController.m
+++ PlayingWithMapboxGL/ViewController.m
@@ -7,8 +7,13 @@
 //
 
 #import "ViewController.h"
+#import <MapboxGL/MapboxGL.h>
+#import <CoreLocation/CoreLocation.h>
+#import "Constants.h"
 
 @interface ViewController ()
+
+
 
 @end
 
@@ -17,6 +22,17 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    /*
+      most important, app will not run without this line
+     */
+    [MGLAccountManager setMapboxMetricsEnabledSettingShownInApp:YES];
+    /*
+     the rest is covered in the tutorial
+     */
+    MGLMapView *mapView = [[MGLMapView alloc] initWithFrame:self.view.bounds accessToken:API_ACCESS_TOKEN];
+    mapView.autoresizingMask = UIViewAutoresizingFlexibleWidth | UIViewAutoresizingFlexibleHeight;
+    [mapView setCenterCoordinate:CLLocationCoordinate2DMake(40.7528, -73.9765) zoomLevel:15 animated:YES];
+    [self.view addSubview:mapView];
 }
 
 - (void)didReceiveMemoryWarning {
